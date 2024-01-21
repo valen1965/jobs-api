30c941d11f5e02e2a953c3ab4ca88753d717fc64
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2024-01-21 15:25:38 +0200

    Updated dummy API and port

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+
+app.get('/', (req, res) => {
+  res.send('jobs api');
+});
+
 // routes;
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
@@ -43,7 +48,7 @@ app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
