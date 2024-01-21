5d86f3e32694128a45c285ae96d1572b32ad3a2d
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2024-01-21 22:59:13 +0200

    swagger UI

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+ 
+
 const express = require('express');
 const app = express();
 
@@ -38,8 +45,10 @@ app.use(xss());
 
 
 app.get('/', (req, res) => {
-  res.send('jobs api');
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes;
 app.use('/api/v1/auth', authRouter);
